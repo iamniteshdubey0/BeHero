5fbf60a7c81d3b134ee389d3d64b479e8685efad
Nitesh <dubeynit333@gmail.com>
2025-04-18 15:29:49 +0530

    Add VerticalTabs component and update Tutorial layout
    - Introduced VerticalTabs component for better navigation.
    - Refactored Tutorial component to integrate VerticalTabs.
    - Cleaned up unused styles and improved layout structure.

--- src/component/Features/index.jsx
+++ src/component/Features/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container, styled, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  styled,
+  Grid,
+  Typography,
+  List,
+  ListItem,
+} from "@mui/material";
 import { tokens } from "../../utils/theme";
 
 const FeatureContainer = styled(Container)(({ theme }) => ({
@@ -13,14 +20,14 @@ const FeatureContainer = styled(Container)(({ theme }) => ({
   marginTop: "40px",
 }));
 
-const Wrapper = styled(Grid)(({ theme }) => ({
+const Wrapper = styled(Grid)(() => ({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  flexDirection:'column',
+  flexDirection: "column",
 }));
 
-const FeatureBox = styled(Grid)(({ theme }) => ({
+const FeatureHighlight = styled(Grid)(({ theme }) => ({
   width: "85%",
   height: "200px",
   backgroundColor:
@@ -31,29 +38,29 @@ const FeatureBox = styled(Grid)(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  marginBottom:'10px'
+  marginBottom: "10px",
 }));
 
-
-const FeatureDiscrption = styled(Grid)(({ theme }) => ({
+const FeatureDetails = styled(Grid)(({ theme }) => ({
   width: "85%",
-  height: "300px",
+  padding: "10px 30px",
   backgroundColor:
     theme.palette.mode === "light"
       ? tokens(theme.palette.mode).yellow[600]
       : tokens(theme.palette.mode).darkPurple[600],
   borderRadius: "20px",
   display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  gap: "5px",
 }));
 
-const Header = styled(Grid)(({ theme }) => ({
-  padding: "0px 30px",
+const SectionHeader = styled(Grid)(() => ({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "flex-start",
+  padding: "0px 30px",
   gap: "12px",
 }));
 
@@ -62,42 +69,9 @@ const Heading = styled(Typography)(({ theme }) => ({
   fontWeight: theme.typography.fontWeightBold,
 }));
 
-const ParaText = styled(Typography)(({ theme }) => ({
+const Subtext = styled(Typography)(({ theme }) => ({
   width: "90%",
   fontSize: theme.typography.body2.fontSize,
-  fontWeight: theme.typography.fontWeightMedium,
-  marginBottom: "10px",
-}));
-
-const FeatureTabs = styled(Grid)(({ theme }) => ({
-  padding: "15px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "flex-start",
-  borderRadius: "10px",
-  cursor: "pointer",
-  gap: "10px",
-
-  "&:hover": {
-    backgroundColor:
-      theme.palette.mode === "light"
-        ? tokens(theme.palette.mode).primary[600]
-        : tokens(theme.palette.mode).primary[600],
-    color: tokens(theme.palette.mode).white[600],
-  },
-}));
-
-const TabIcon = styled(Grid)((theme) => ({}));
-
-const TabTitle = styled(Typography)(({ theme }) => ({
-  fontSize: theme.typography.h5.fontSize,
-  fontWeight: theme.typography.fontWeightMedium,
-}));
-
-const TabText = styled(Typography)(({ theme }) => ({
-  width: "85%",
-  fontSize: "10px",
   fontWeight: theme.typography.fontWeightMedium,
   marginBottom: "10px",
 }));
@@ -113,59 +87,196 @@ const DividerY = styled("div")(({ theme }) => ({
   borderRadius: "4px",
 }));
 
+const FeatureTab = styled(Grid)(({ theme }) => ({
+  padding: "15px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  borderRadius: "10px",
+  cursor: "pointer",
+  gap: "10px",
+  "&:hover": {
+    backgroundColor: tokens(theme.palette.mode).primary[600],
+    color: tokens(theme.palette.mode).white[600],
+  },
+}));
+
+const IconBox = styled("img")(() => ({
+  width: "60px",
+  height: "60px",
+}));
+
+const TabTitle = styled(Typography)(({ theme }) => ({
+  fontSize: theme.typography.h5.fontSize,
+  fontWeight: theme.typography.fontWeightMedium,
+}));
+
+const TabSubtext = styled(Typography)(({ theme }) => ({
+  fontSize: "10px",
+  fontWeight: theme.typography.fontWeightMedium,
+}));
+
+const FeatureCard = styled(Grid)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 0,
+}));
+
+const CardHeader = styled(Grid)(() => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "6px",
+}));
+
+const CardImage = styled("img")(() => ({
+  width: "35px",
+  height: "35px",
+}));
+
+const StyledList = styled(List)(() => ({
+  padding: "15px",
+}));
+
+const StyledListItem = styled(ListItem)(() => ({
+  display: "list-item",
+  listStyleType: "disc",
+  padding: "2px 0",
+  fontSize: "12px",
+}));
+
 const Features = () => {
   return (
     <FeatureContainer maxWidth="lg">
       <Wrapper container>
-        <FeatureBox>
-          <Header size={{ xs: 12, sm: 3.5 }}>
+        <FeatureHighlight>
+          <SectionHeader size={{ xs: 12, sm: 3.5 }}>
             <Heading>Features</Heading>
-            <ParaText>
+            <Subtext>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed iure
               ea cupiditate, a
-            </ParaText>
-          </Header>
-          <DividerY></DividerY>
-          <FeatureTabs size={{ xs: 12, sm: 2.5 }}>
-            <TabIcon>
-              <img
-                width="60"
-                height="60"
-                src="https://img.icons8.com/nolan/50/idea.png"
-                alt="idea"
-              />
-            </TabIcon>
-            <TabTitle>Creative Idea</TabTitle>
-            <TabText>Yeha hai Idea</TabText>
-          </FeatureTabs>
-          <FeatureTabs size={{ xs: 12, sm: 2.5 }}>
-            <TabIcon>
-              <img
-                width="60"
-                height="60"
-                src="https://img.icons8.com/nolan/50/book.png"
-                alt="book"
-              />
-            </TabIcon>
-            <TabTitle>Creative Idea</TabTitle>
-            <TabText>Yeha hai Idea</TabText>
-          </FeatureTabs>
-          <FeatureTabs size={{ xs: 12, sm: 2.5 }}>
-            <TabIcon>
-              <img
-                width="60"
-                height="60"
-                src="https://img.icons8.com/nolan/50/product.png"
-                alt="product"
-              />
-            </TabIcon>
-            <TabTitle>Creative Idea</TabTitle>
-            <TabText>Yeha hai Idea</TabText>
-          </FeatureTabs>
-        </FeatureBox>
-        <FeatureDiscrption>
-          {/* Add more details of feature here */}
-        </FeatureDiscrption>
+            </Subtext>
+          </SectionHeader>
+
+          <DividerY />
+
+          <FeatureTab size={{ xs: 12, sm: 2.5 }}>
+            <IconBox
+              src="https://img.icons8.com/color/96/fantasy.png"
+              alt="idea"
+            />
+            <TabTitle>Story Personalization</TabTitle>
+            <TabSubtext>Every Hero Deserves a Personal Touch</TabSubtext>
+          </FeatureTab>
+
+          <FeatureTab size={{ xs: 12, sm: 2.5 }}>
+            <IconBox
+              src="https://img.icons8.com/color/96/courses.png"
+              alt="book"
+            />
+            <TabTitle>Pre-Defined Story</TabTitle>
+            <TabSubtext>Ready-Made Adventures, Infinitely Magical</TabSubtext>
+          </FeatureTab>
+
+          <FeatureTab size={{ xs: 12, sm: 2.5 }}>
+            <IconBox
+              src="https://img.icons8.com/color/96/keyhole-shield.png"
+              alt="product"
+            />
+            <TabTitle>Data Security & Privacy</TabTitle>
+            <TabSubtext>Safe Stories, Happy Parents</TabSubtext>
+          </FeatureTab>
+        </FeatureHighlight>
+
+        <FeatureDetails container>
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>AI Story Generation</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                A unique, personalized superhero story
+              </StyledListItem>
+              <StyledListItem>
+                Always creative, fun, and age-appropriate
+              </StyledListItem>
+            </StyledList>
+          </FeatureCard>
+
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>Download as PDF</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                nstantly download as a print-ready PDF
+              </StyledListItem>
+              <StyledListItem>
+                Easy to read, share, or save as keepsake
+              </StyledListItem>
+            </StyledList>
+          </FeatureCard>
+
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>Illustrated Scenes</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                Generates superhero stories with AI.
+              </StyledListItem>
+              <StyledListItem>
+                Personalizes content based on input.
+              </StyledListItem>
+            </StyledList>
+          </FeatureCard>
+
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>Print-on-Demand Book</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                Order a printed, high-quality book.
+              </StyledListItem>
+              <StyledListItem>Delivered right to your home.</StyledListItem>
+            </StyledList>
+          </FeatureCard>
+
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>Family & Friends Integration</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                Add parents, siblings, and friends to the story.
+              </StyledListItem>
+              <StyledListItem>
+                Makes every tale feel warm and personal
+              </StyledListItem>
+            </StyledList>
+          </FeatureCard>
+
+          <FeatureCard size={{ xs: 12, sm: 3.9 }}>
+            <CardHeader>
+              <CardImage src="https://img.icons8.com/color/96/artificial-intelligence.png" />
+              <TabTitle>Illustrated Scenes</TabTitle>
+            </CardHeader>
+            <StyledList>
+              <StyledListItem>
+                Generates superhero stories with AI.
+              </StyledListItem>
+              <StyledListItem>
+                Personalizes content based on input.
+              </StyledListItem>
+            </StyledList>
+          </FeatureCard>
+        </FeatureDetails>
       </Wrapper>
     </FeatureContainer>
   );
--- src/component/Tutorial/Tutorial.jsx
+++ src/component/Tutorial/Tutorial.jsx
@@ -1,41 +1,21 @@
 import React from "react";
 import { Container, Box, Grid, styled } from "@mui/material";
 import { tokens } from "../../utils/theme";
+import VerticalTabs from "../Elements/VerticalTabs";
 
 const TutorialContainer = styled(Container)(({ theme }) => ({
   display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
   padding: "5px",
   width: "100%",
   height: "70vh",
   overflow: "hidden",
   marginTop: "40px",
-  position:'relative',
-  backgroundColor:
-    theme.palette.mode === "light"
-      ? tokens(theme.palette.mode).secondary[500]
-      : tokens(theme.palette.mode).secondary[600],
-}));
-
-const OverlayWrapper = styled(Grid)(({ theme }) => ({
-  top: 0,
-  left: 0,
-  position: "absolute",
-  display: "flex",
-  width: "100%",
-  justifyContent: "center",
-  alignItems: "center",
-  zIndex: -1,
 }));
 
 const ContentWrapper = styled(Grid)(({ theme }) => ({
   display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
   flexDirection: "column",
-  position: "relative",
-  zIndex:10,
+  padding: "20px",
 }));
 
 const TutorialTabBox = styled(Grid)(({ theme }) => ({
@@ -47,10 +27,10 @@ const TutorialTabBox = styled(Grid)(({ theme }) => ({
 
 const Tutorial = () => {
   return (
-    <TutorialContainer maxWidth="xl">
-      <OverlayWrapper>overlay</OverlayWrapper>
+    <TutorialContainer maxWidth="lg">
       <ContentWrapper>
-        <TutorialTabBox>How tio</TutorialTabBox>
+        {/* <TutorialTabBox>How tio</TutorialTabBox> */}
+        <VerticalTabs></VerticalTabs>
       </ContentWrapper>
     </TutorialContainer>
   );
